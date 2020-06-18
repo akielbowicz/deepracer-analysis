a7de9e03da012b029dd06bcb578f19470e3bce5e
Augusto <augusto.kiel@gmail.com>
2020-06-18 11:08:48 -0300

    add v1 reward function

--- reward/awsreward.py
+++ reward/awsreward.py
@@ -282,7 +282,36 @@ class RewardW6(RewardW5):
 class RewardW7(RewardW6):
     pass
 
-class RewardW8(RewardW7):
+class RewardV1(RewardW7):
+    
+    max_progress = 100.0 # per doc
+    max_speed    = 4.0
+    max_steering = 15.0
+    min_progress_for_progress_reward = 20.0 # once car covers at least 20% of track we give it a high reward depending on steps
+
+    
+        # optimal steps proportional to the progress so far (there is an assumption on even distribution of steps across the track
+    def optimal_steps(self):
+        # make the optimal number of steps 20% more conservative than what we calculate based on length
+        optimal_error = 1.20
+        
+        #15 steps per second as camera has 15 frames per second
+        return self.progress / 100 * optimal_error * self.track_length / self.max_speed * 15 
     
     def complete_track(self):
-        return 1.0
+        # we only give out reward past the minimum
+        if self.progress < self.min_progress_for_progress_reward:
+            return 0.0
+        
+        step_diff = self.optimal_steps() - self.steps
+        
+        # make the reward much steeper than just a simple ratio
+        step_reward_ratio = pow(1000, step_diff / self.optimal_steps())
+        
+        # max make the max step reward depend on the optimal steps so this it depends on the track length (and not fixed to 100
+        # for example) it shouldnt be possible for the model to get this type of reward unless it completes the track though 
+        max_step_reward = self.optimal_steps() * 10
+        
+        return step_reward_ratio * max_step_reward     
+
+    
